b4c7e5d46c98b85826ba4fa2d2c356f39bc00c69
Muhammad Bilal Bangash <muhammadbilalbangash@hotmail.com>
2024-05-21 00:24:46 +0500

    client and server components

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,8 @@
 import { Inter } from 'next/font/google'
 import Header from '@/components/layout/header'
-import './globals.css'
 import Footer from '@/components/layout/footer'
+import Providers from './providers'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,11 +13,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
+        <Providers>
+          <Header />
+          {children}
+          <Footer />
+        </Providers>
       </body>
     </html>
   )
